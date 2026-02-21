1c7babe6c236e012ef75829cbf409e5d7cdb920d
Hyoga Fukuno <hyouga.dev@gmail.com>
2026-02-22 02:16:12 +0900

    fix

---
+++ src/AsyncStateMachine.Tests/DummyContext.cs
@@ -0,0 +1,5 @@
+namespace AsyncStateMachine.Tests;
+
+public class DummyContext
+{
+}
---
+++ src/AsyncStateMachine/State.cs
@@ -0,0 +1,30 @@
+namespace AsyncStateMachine;
+
+public abstract class State<TContext> : IDisposable
+{
+    protected IReadOnlyStateMachine<TContext>? stateMachine;
+    bool disposed;
+
+    protected internal virtual bool CanBeTransition(TContext context) => true;
+    protected virtual void OnInitialize(TContext context) { }
+    protected virtual ValueTask OnExecuteAsync(TContext context, CancellationToken ct) => default;
+    protected virtual void OnDispose() { }
+
+    public void Initialize(IReadOnlyStateMachine<TContext> machine, TContext context)
+    {
+        stateMachine = machine;
+        OnInitialize(context);
+    }
+
+    public async ValueTask ExecuteAsync(TContext context, CancellationToken ct) 
+        => await OnExecuteAsync(context, ct);
+
+    public void Dispose()
+    {
+        if (disposed) { return; }
+
+        disposed = true;
+        OnDispose();
+        GC.SuppressFinalize(this);
+    }
+}
